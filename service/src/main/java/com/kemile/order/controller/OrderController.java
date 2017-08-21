51c4fbd1a52c7687997982314eed56b1f7e8732a
danceforyou <danceforyou@163.com>
2017-08-21 22:42:20 +0800

    订单审核功能实现

--- service/src/main/java/com/kemile/order/controller/OrderController.java
+++ service/src/main/java/com/kemile/order/controller/OrderController.java
@@ -3,6 +3,10 @@ package com.kemile.order.controller;
 import com.kemile.common.restful.exception.BizException;
 import com.kemile.common.restful.exception.BizExceptionEnum;
 import com.kemile.common.utils.DateUtil;
+import com.kemile.common.utils.Global;
+import com.kemile.management.domain.Manager;
+import com.kemile.management.service.ManagerService;
+import com.kemile.management.service.impl.ManagerServiceImpl;
 import com.kemile.order.domain.Order;
 import com.kemile.order.service.impl.OrderServiceImpl;
 import com.kemile.user.domain.User;
@@ -10,10 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017/8/9 0009.
@@ -26,11 +29,19 @@ public class OrderController {
     @Autowired
     private OrderServiceImpl orderService;
 
+    @Autowired
+    private ManagerServiceImpl managerService;
+
+    @Autowired
+    HttpSession session;
+
     /**
      * 用户列表
      * @param keyword   查询关键字
      * @param sort      排序字段
      * @param start     起始条数
+     * @param order_status  订单状态（支付状态）
+     * @param aduit_status  审核状态
      * @param limit
      * @return
      */
@@ -39,12 +50,16 @@ public class OrderController {
     @RequestMapping(value = "OrderList", method = RequestMethod.GET)
     public Map<String, Object> OrderList(
             @RequestParam(value = "keyword") final String keyword,
+            @RequestParam(value = "order_status") final String order_status,
+            @RequestParam(value = "aduit_status") final String aduit_status,
             @RequestParam(value = "sort") final String sort,
             @RequestParam(value = "start", required = true) final String start,
             @RequestParam(value = "limit", required = true) final String limit) {
         final Map<String, Object> condition = new HashMap<String, Object>() {
             {
                 put("keyword", keyword.equals("") ? null : keyword);
+                put("order_status", order_status.equals("-1") ? null : order_status);
+                put("aduit_status", aduit_status.equals("-1") ? null : aduit_status);
                 put("sort", sort.equals("") ? "id" : sort);
                 put("start", start);
                 put("limit", limit);
@@ -119,15 +134,25 @@ public class OrderController {
     @RequestMapping(value = "UpdateStatus")
     public boolean updateStatus(
             @RequestParam(value = "id") final String id,
-            @RequestParam(value = "order_status") final String order_status,
-            @RequestParam(value = "aduit_status") final String aduit_status,
-            @RequestParam(value = "service_status") final String service_status){
+            @RequestParam(value = "status") final int status,
+            @RequestParam(value = "type") final int type){
+        //final Manager manager = managerService.findOne("id", operatorId);
+       final Manager manager = (Manager) session.getAttribute(Global.QR_MANAGER);
         final Map<String, Object> condition = new HashMap<String, Object>() {
             {
                 put("id", id.equals("") ? null : Integer.parseInt(id));
-                put("order_status", order_status.equals("") ? null : Integer.parseInt(order_status));
-                put("aduit_status", aduit_status.equals("") ? null : Integer.parseInt(aduit_status));
-                put("service_status", service_status.equals("") ? null : Integer.parseInt(service_status));
+                if (type == 0){//更还订单状态
+                    put("order_status", status);
+                }else if (type == 1){//更改审核状态
+                    put("aduit_status", status);
+                    put("check_man", manager.getId());
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                    //System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+                    put("check_time", df.format(new Date()));
+                }else if (type == 2){//更改服务状态
+                    put("service_status", status);
+                    put("service_manager", manager.getId());
+                }
             }
         };
         boolean result = orderService.updateStatus(condition);
